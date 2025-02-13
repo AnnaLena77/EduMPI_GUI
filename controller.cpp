3755bc2ec4149a15f32d4508dcb0fce7081887b2
MarvinZein <127395156+MarvinZein@users.noreply.github.com>
2025-02-13 13:43:22 +0100

    strip an additional / from the prefix of the program's file path on windows for a correct path

--- controller.cpp
+++ controller.cpp
@@ -67,6 +67,23 @@ Controller::~Controller()
 
 }
 
+QString removeFilePrefix(const QString &file_path){
+    QString adjustedPath = file_path;
+    if (adjustedPath.startsWith("file:///"))
+    {
+        if (QOperatingSystemVersion::currentType() == QOperatingSystemVersion::Windows)
+        {
+            adjustedPath.remove(0, 8);
+        }
+        else
+        {
+            adjustedPath.remove(0, 7);
+        }
+    }
+
+    return adjustedPath;
+}
+
 bool Controller::copyEnvFile(){
     char tempFileTemplate[] = "/tmp/XXXXXX.env";
     int fd = mkstemps(tempFileTemplate, 4);
@@ -216,9 +233,7 @@ void Controller::writeLocalBashFile(QString local_path, bool file, int proc_num)
     }
     emit setProcNum(proc_num);
 
-    if(local_path.startsWith("file:///")){
-        local_path.remove(0,7);
-    }
+    local_path = removeFilePrefix(local_path);
 
     QProcess bash_proc;
     QString resourcePath;
@@ -287,9 +302,8 @@ void Controller::writeLocalBashFile(QString local_path, bool file, int proc_num)
 }
 
 bool Controller::checkFile(QString source, QString program, bool file){
-    if(source.startsWith("file:///")){
-        source.remove(0,7);
-    }
+    source = removeFilePrefix(source);
+
     if(file){
         if(source.endsWith(".c")){
             QFile fileA(source);
