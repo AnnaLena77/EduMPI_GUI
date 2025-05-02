e25d863f4075c8d0f1fab4493e886ba33de78024
MarvinZein <127395156+MarvinZein@users.noreply.github.com>
2025-05-02 16:15:33 +0200

    Copy postgres dlls over and set the correct postgres root

--- windows_deployment.bat
+++ windows_deployment.bat
@@ -9,3 +9,8 @@ C:\Qt\Tools\Ninja\ninja.exe
 C:\Qt\6.8.1\mingw_64\bin\windeployqt.exe --qmldir .. --dir release appGUI_Cluster.exe
 
 copy appGUI_Cluster.exe release
+copy "C:\msys64\mingw64\bin\libpq.dll" release
+copy "C:\msys64\mingw64\bin\libssl-3-x64.dll" release
+copy "C:\msys64\mingw64\bin\libcrypto-3-x64.dll" release
+copy "C:\msys64\mingw64\bin\libintl-8.dll" release
+copy "C:\msys64\mingw64\bin\libiconv-2.dll" release
